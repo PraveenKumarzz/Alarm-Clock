6ab6816954ebd991a93409ca91aa977974e3c75d
Praveen Kumar <101964246+PraveenKumarzz@users.noreply.github.com>
2022-11-13 14:10:44 +0530

    Create Alarm.java

---
+++ Alarm.java
@@ -0,0 +1,24 @@
+package com.clock;
+
+import java.text.*;
+import java.util.*;
+
+public class Alarm extends time{
+	
+	public static void main(String[] args) {
+		
+	     
+		
+		
+	       
+	     Alarm ob=new Alarm();
+	 
+	     ob.input();
+	     ob.secconvert();
+	     ob.start();
+	     
+
+		
+		
+	}
+}
